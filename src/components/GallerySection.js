22726cefa908795f17df8518e16ea2dd679138ee
ip99202 <ip99202@naver.com>
2024-11-15 00:34:49 +0900

    갤러리 슬라이드 효과 제거

--- src/components/GallerySection.js
+++ src/components/GallerySection.js
@@ -171,11 +171,10 @@ function GallerySection() {
   const settings = {
     dots: true,
     infinite: true,
-    speed: 2000,
+    speed: 500,
     slidesToShow: 1,
     slidesToScroll: 1,
-    autoplay: true,
-    autoplaySpeed: 5000,
+    autoplay: false,
     pauseOnHover: true,
     nextArrow: <NextArrow />,
     prevArrow: <PrevArrow />,
